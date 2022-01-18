ca3f3b2f05f0b8a80b180d1d6fdc0083d2ee6ace
Calal B. Manafi <Calal.Manafi@kapitalbank.az>
2022-01-18 11:23:34 +0400

    Added message to README file for FIREBASE config

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,51 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+
+import { auth } from "../firebase";
+import Home from "../views/Home.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: {
+      requiresAuth: true,
+    },
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue"),
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.path === "/login" && auth.currentUser) {
+    next("/");
+    return;
+  }
+  if (
+    to.matched.some((record) => record.meta.requiresAuth) &&
+    !auth.currentUser
+  ) {
+    next("/login");
+    return;
+  }
+
+  next();
+});
+
+export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,97 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+import router from "../router";
+import { auth } from "../firebase";
 
 export default createStore({
   state: {
+    user: null,
   },
   mutations: {
+    setUser(state, user) {
+      state.user = user;
+    },
+    deleteUser(state) {
+      state.user = null;
+    },
   },
   actions: {
+    async login({ commit }, user) {
+      const { email, password } = user;
+
+      try {
+        await signInWithEmailAndPassword(auth, email, password);
+      } catch (error) {
+        switch (error.code) {
+          case "auth/user-not-found":
+            alert("User not found");
+            break;
+          case "auth/wrong-password":
+            alert("Wrong password");
+            break;
+          default:
+            alert("Something went wrong");
+        }
+
+        return;
+      }
+
+      commit("setUser", auth.currentUser);
+      router.push("/");
+    },
+    async register({ commit }, user) {
+      const { email, password } = user;
+
+      try {
+        await createUserWithEmailAndPassword(auth, email, password);
+      } catch (error) {
+        switch (error.code) {
+          case "auth/email-already-in-use":
+            alert("Email already in use");
+            break;
+          case "auth/invalid-email":
+            alert("Invalid email");
+            break;
+          case "auth/operation-not-allowed":
+            alert("Operation not allowed");
+            break;
+          case "auth/weak-password":
+            alert("Weak password");
+            break;
+          default:
+            alert("Something went wrong");
+        }
+
+        return;
+      }
+
+      commit("setUser", auth.currentUser);
+      router.push("/");
+    },
+    async logout({ commit }) {
+      await signOut(auth);
+      commit("deleteUser");
+      router.push("/login");
+    },
+
+    fetchUser({ commit }) {
+      auth.onAuthStateChanged(async (user) => {
+        if (user === null) {
+          commit("deleteUser");
+        } else {
+          commit("setUser", user);
+
+          if (router.isReady() && router.currentRoute.value.path === "/login") {
+            router.push("/");
+          }
+        }
+      });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
